be3d21e8be1f1c7e2415f4990be27d7560aa8032
Tan Zi Xuan <zxtan97@hotmail.com>
2023-10-13 02:12:16 +0000

    Create models

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'UNIQUE_KEY',
+        'PRODUCT_TITLE',
+        'PRODUCT_DESCRIPTION',
+        'STYLE#',
+        'SANMAR_MAINFRAME_COLOR',
+        'SIZE',
+        'COLOR_NAME',
+        'PIECE_PRICE',
+    ];
+}
